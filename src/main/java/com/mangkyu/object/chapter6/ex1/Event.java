c5f54aa76d15aea37ed2f7a4f813861b41478ff0
MangKyu <whalsrb1226@gmail.com>
2024-05-19 16:27:22 +0900

    [CHAPTER6] 명령-쿼리 분리 원칙(명령과 쿼리를 분리하지 않아 부수 효과가 생김)

---
+++ src/main/java/com/mangkyu/object/chapter6/ex1/Event.java
@@ -0,0 +1,39 @@
+package com.mangkyu.object.chapter6.ex1;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
+
+public class Event {
+
+    private String subject;
+    private LocalDateTime from;
+    private Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public boolean isSatisfied(RecurringSchedule schedule) {
+        if (from.getDayOfWeek() != schedule.getDayOfWeek()
+            || !from.toLocalTime().equals(schedule.getFrom())
+            || ! duration.equals(schedule.getDuration())
+        ) {
+            reschedule(schedule);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void reschedule(RecurringSchedule schedule) {
+        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom());
+        duration = schedule.getDuration();
+    }
+
+    private long daysDistance(RecurringSchedule schedule) {
+        return schedule.getDayOfWeek().getValue() - from.getDayOfWeek().getValue();
+    }
+}
---
+++ src/test/java/com/mangkyu/object/chapter6/ex1/EventTest.java
@@ -0,0 +1,7 @@
+package com.mangkyu.object.chapter6.ex1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+}
